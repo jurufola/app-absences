b9359da081266c1f849aa37c4baab1b4be1f6e6b
jurufola <moulaye.haidara@laposte.net>
2021-08-20 16:12:32 +0200

    Ajout composante VacationRequestComponent

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VacationRequestComponent } from './components/vacation-request/vacation-request.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VacationRequestComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-client/src/app/components/vacation-request/vacation-request.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vacation-request',
+  templateUrl: './vacation-request.component.html',
+  styleUrls: ['./vacation-request.component.css']
+})
+export class VacationRequestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
