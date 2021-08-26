4fce24d981cdc2a3d334dbdba2ad08cc79ddb8c2
diffethi <58692855+diffethi@users.noreply.github.com>
2021-08-26 17:11:25 +0200

    commit a completer

--- angular-client/src/app/app-routing.module.ts
+++ angular-client/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path:"/home", component:HomeComponent}];
+  {path:"", component:HomeComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
