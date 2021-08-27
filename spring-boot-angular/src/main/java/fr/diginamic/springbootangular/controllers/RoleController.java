5dda26961bfe5f3951b5c1c6f5be7ac607c72959
Mathis AGBANRIN <mathis.agb@gmail.com>
2021-08-27 15:26:13 +0200

    add missing @RestController

--- spring-boot-angular/src/main/java/fr/diginamic/springbootangular/controllers/RoleController.java
+++ spring-boot-angular/src/main/java/fr/diginamic/springbootangular/controllers/RoleController.java
@@ -3,7 +3,9 @@ package fr.diginamic.springbootangular.controllers;
 import fr.diginamic.springbootangular.services.RoleService;
 import fr.diginamic.springbootangular.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class RoleController {
     @Autowired
     RoleService roleService;
