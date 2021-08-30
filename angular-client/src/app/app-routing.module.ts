00a0a427c31ea8772dc821656bff1cc5dc6847cb
Mathis AGBANRIN <mathis.agb@gmail.com>
2021-08-30 10:45:21 +0200

    clear useless stuff

--- angular-client/src/app/app-routing.module.ts
+++ angular-client/src/app/app-routing.module.ts
@@ -1,20 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AdminComponent } from './components/admin/admin.component';
 import { AuthenticationComponent } from './components/authentication/authentication.component';
-import { HomeComponent } from './components/home/home.component';
-import { ManagerComponent } from './components/manager/manager.component';
-import { UserComponent } from './components/user/user.component';
 import { AuthenticationGuard } from './services/guards/authentication.guard';
 
 const routes: Routes = [
 
   { path: '', pathMatch: 'full', redirectTo: 'authentication'}, // redirection all to th
-  { path: 'home', component: HomeComponent},// path for all users like employes, manager, admin
   { path: 'authentication', component: AuthenticationComponent }, // path for authentication
-  { path: 'user', component: UserComponent},// path for user like employes
-  { path: 'admin', component: AdminComponent, canActivate: [AuthenticationGuard] }, // path for administrator
-  { path: 'manager', component: ManagerComponent, canActivate: [AuthenticationGuard] } // path for manager
 
 ];
 
