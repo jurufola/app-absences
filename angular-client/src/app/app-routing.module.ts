baeac4c4e6a87fe13f9863027ffee3be7a358e80
sagab77 <amh.gabi@gmail.com>
2021-08-31 17:54:11 +0200

    Nettoyage branche

--- angular-client/src/app/app-routing.module.ts
+++ angular-client/src/app/app-routing.module.ts
@@ -1,14 +1,11 @@
 import { GestionAbsencesComponent } from './components/gestion-absences/gestion-absences.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { TestComponent} from './test/test.component';
+
 
 const routes: Routes = [
-  {path: 'accueil', component: TestComponent},
-  {path: 'gestion des absences', component: TestComponent},
-  {path: 'planning des absences', component: TestComponent},
-  {path: 'jours feriés', component: TestComponent},
-  {path: 'se déconecter', component: TestComponent},
+  {path: 'gestion_des_absences', component: GestionAbsencesComponent},
+  
 ];
 
 @NgModule({
