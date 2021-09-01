ed4973354fae9aff205d89c82a3f846c94b044c2
Mathis AGBANRIN <mathis.agb@gmail.com>
2021-09-01 17:53:25 +0200

    add missing attributes for closed days

--- angular-client/src/app/models/closed-day.ts
+++ angular-client/src/app/models/closed-day.ts
@@ -1,7 +1,9 @@
-enum Category { JOUR_FERIE, RTT_EMPLOYEUR };
-
+export enum Category { JOUR_FERIE, RTT_EMPLOYEUR };
+export enum JourSemaine { LUNDI, MARDI, MERCREDI, JEUDI, VENDREDI, SAMEDI, DIMANCHE }
 export class ClosedDay {
   id: number;
   date: Date;
   category: Category;
+  jour: JourSemaine;
+  commentaire: string;
 }
