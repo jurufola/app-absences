c7b27f7115bf27ddc27cd5e3272eecfc511e195b
jurufola <moulaye.haidara@laposte.net>
2021-09-01 19:05:19 +0200

    Menage branche moulaye-dev sur app-routing.module et app.component.html

--- angular-client/src/app/app-routing.module.ts
+++ angular-client/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VacationRequestComponent } from './components/vacation-request/vacation-request.component';
 
 const routes: Routes = [
-
+  {path: 'demande-abscence', component: VacationRequestComponent}
 ];
 
 @NgModule({
